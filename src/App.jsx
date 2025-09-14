50f15a434b76741fff1c939ff0387a6bdcc638a0
himanshu <khatri.hk18@gmail.com>
2025-09-15 02:10:31 +0530

    Add new NavBar reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import "./App.css";
-import MovieCard from "./components/MovieCard";
+import "./css/App.css";
+import {Routes, Route} from "react-router-dom";
 import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
-    <Home />
-    {/* <Favorites /> */}
-    </>
+    <div>
+      <header className="navbar">
+        <NavBar />
+      </header>
+
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
